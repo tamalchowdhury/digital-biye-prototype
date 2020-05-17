8ff6ce361bc833dfa818e0f8bffddb3fd7364553
Tamal Anwar <tamalanwaronline@gmail.com>
2020-05-17 18:24:02 +0600

    bootstrapping from existing website

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ require('dotenv').config({ path: 'variables.env' });
 mongoose.connect(process.env.DATABASE);
 mongoose.Promise = global.Promise;
 mongoose.connection.on('error', (err) => {
-	console.log('We have an error with the database: ' + err);
-})
+  console.log('We have an error with the database: ' + err);
+});
 
 // Express session
 app.use(session({ secret: process.env.SECRET }));
@@ -27,7 +27,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // Setting up views
-app.set('views', (__dirname + '/views'));
+app.set('views', __dirname + '/views');
 app.set('view engine', 'pug');
 
 // Public folder
@@ -43,8 +43,8 @@ app.use(expressValidator());
 
 // Locals
 app.use((req, res, next) => {
-	res.locals.user = req.user || null;
-	next();
+  res.locals.user = req.user || null;
+  next();
 });
 
 // Setting up the routes
@@ -53,4 +53,6 @@ app.use('/', routes);
 const PORT = process.env.PORT || 3000;
 
 // Starting the server
-app.listen(PORT, () => console.log('We have a server running on PORT: ' + PORT));
+app.listen(PORT, () =>
+  console.log('We have a server running on PORT: ' + PORT)
+);
